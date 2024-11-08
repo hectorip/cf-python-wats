6de678e4ccb1d2ecfe6bfca85baf40e584633e36
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2024-11-07 23:42:01 -0600

    WIP

--- problems/10.py
+++ problems/10.py
@@ -1,10 +1,1 @@
 # Procesos e hilos
-from multiprocessing import Process
-
-
-def task(n):
-    return n * n
-
-
-if __name__ == "__main__":
-    processes = [Process(target=task, args=(i,)) for i in range(10)]
